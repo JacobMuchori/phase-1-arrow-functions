87a05d91057f84eb8af58520398495407be4e399
JacobMuchori <muskins18@gmail.com>
2022-11-22 21:50:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(y=2000) {
+    return(y/100);
+}
+console.log(divide());
+
+let x=2;
+const square= (x)=> {
+    return(x*x);
+}
+console.log(square(x));
+
+let a = 3;
+let b = 4;
+const add= (a, b)=> {
+    return(a + b);
+}
+console.log(add(a, b));
